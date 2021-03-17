abdb0f0b5bfa99b1486ffd8a2c72328603bda371
Monty64 <63143383+Monty64@users.noreply.github.com>
2021-03-17 14:12:22 +1300

    Update Functions.ino

--- Functions/Functions.ino
+++ Functions/Functions.ino
@@ -20,11 +20,6 @@ const byte left_back_sensor = A4;
 const byte back_sensor = A11;
 const byte front_sensor = A15;
 
-//byte long1 = 1;
-//byte short1 = 1;
-//byte long2 = 1;
-//byte short2 = 1;
-//byte front, back, left, right, left_front, left_rear, right_rear, right_front;
 int speed_val;
 void setup(void){
   stop();
@@ -32,146 +27,96 @@ void setup(void){
   SerialCom ->begin(115200);
   SerialCom ->println("Starting");
   enable_motors();
-  orientation2();
+  orientation();
   stop();
 }
 void loop(void){}
 
 float left_back_dist (void){
-  //Replace with derived formula
+  //Derived formula
   int ADCval = analogRead(left_back_sensor);
   float dist = 1/(0.0005*ADCval - 0.0078);   //11.724/ADCval *(5/1023.0) - 0.24;
   return dist; //in cm
 }
 
 float left_front_dist (void){
-  //Replace with derived formula
+  //Derived formula
   int ADCval = analogRead(left_front_sensor);
   float dist = 1/(0.0005*ADCval - 0.0101);   //11.724/ADCval *(5/1023.0) - 0.24;
   return dist; //in cm
 }
 
 float front_dist (void){
-  //Replace with derived formula
+  //Derived formula
   int ADCval = analogRead(front_sensor);
   float dist = 1/(0.0002*ADCval - 0.0068);  //20.9/ADCval *(5/1023.0);
   return dist; //in cm
 }
 
-
 float back_dist (void){
-  //Replace with derived formula
+  //Derived formula
   int ADCval = analogRead(back_sensor);
   float dist = 1/(0.0002*ADCval - 0.0051);   //20.9/ADCval *(5/1023.0);
   return dist; //in cm
 }
 
-
 //INITIALISING
-//void orientation (){
-//  // Check readings, one will be max range, lowest voltage as voltage is inverse to distance
-//  float diff = 10;
-//  float gain = 10000;
-//  float left_gain = 100;
-////  int long1ADC = analogRead(long1);
-////  int long2ADC = analogRead(long2);
-////  int short1ADC = analogRead(short1);
-////  int short2ADC = analogRead(short2);
-//
-////  minLong =  long1ADC < long2ADC ? long1 : long2;
-////  minShort = short1ADC < short2ADC ? short1 : short2;
-//
-//  //Set the orientation
-//  //Adjust thresholds
-///*  if ((short1ADC < 100) || (long1ADC < 100)){
-//    front = long1;
-//    back = long2;
-//    left = short2;
-//    right = short1;
-//    left_front = 46;
-//    left_rear = 47;
-//    right_rear = 50;
-//    right_front = 51;
-//  } else {
-//    front = long2;
-//    back = long1;
-//    left = short1;
-//    right = short2;
-//    left_front = 50;
-//    left_rear = 51;
-//    right_rear = 46;
-//    right_front = 47;
-//  }*/
-//  
-//  float priorBackDist = back_dist();
-//  float priorLeftDist = left_dist();
-//  float leftDist;
-//  speed_val = 200;
-//
-//  while((diff > 0.5*2) || (diff < -0.5*2)){
-//    cw();
-//    delay(100);
-//    float backDist = back_dist();
-//    leftDist = left_dist();
-//
-//    diff = priorBackDist + priorLeftDist - backDist - leftDist;
-//    speed_val = gain * diff;
-//    priorBackDist = backDist;
-//    priorLeftDist = leftDist;
-//    SerialCom ->println(diff);
-//  }
-//  SerialCom ->println("Moving left");
-//
-//  while((leftDist > 15.2) || (leftDist < 14.8)){
-//    left();
-//    delay(10);
-//    leftDist = left_dist();
-//    SerialCom ->println(leftDist);
-//    speed_val = left_gain * (leftDist - 15);
-//  }
-//  stop();
-//}
-
-void orientation2 (void) {
+void orientation (void) {
   float separationDist = 19;
   float leftFrontDist, leftBackDist, dist; 
   float error = 6.2;
   float angle = 3.14/2;
 
-  int ccwturn, strafeRight;
-  int ccwgain = 1500;
+  int ccwTurn, strafeRight, frontControl, rearControl, tinit;
+  int ccwGain = 1500;
   int strafeGain = 50;
   int t = 0;
-  while(((angle > 0.0349) && (error > 0.5)) || (t < 2000)){
-    
+  
+  while(((abs(angle) > 0.0349) || (abs(error) > 0.2)) && (t < 2000)){
     leftFrontDist = left_front_dist();
     leftBackDist = left_back_dist();
+    //distance from the wall
     dist = (leftFrontDist + leftBackDist)/2;
     //approximate sin theta to theta
     angle = (leftFrontDist - leftBackDist)/separationDist;
 
     if ((angle < 3.14/8) && (angle > -3.14/8)) {
-      //Reference angle is 0
-      ccwturn = angle * ccwgain;
+      //Reference angle is 0, control signal for turning CCW
+      ccwTurn = angle * ccwGain;
       //6.2 = 15 - 8.8 (dist from centre of robot to sensor)
       error = 6.2 - dist;
+      //Control signal for strafing right
       strafeRight = error * strafeGain ;
       strafeRight = (strafeRight > 500) ? 500 : strafeRight;
     } else{
-      ccwturn = 500;
+      ccwTurn = 500;
       strafeRight = 0;
     }
+
+    //Control signal for front motors
+    frontControl = (-ccwTurn + strafeRight > 500) ? 500 : -ccwTurn + strafeRight;
+    frontControl = (-ccwTurn + strafeRight < -500) ? -500 : frontControl;
+
+    //Control signal for rear motors
+    rearControl = (-ccwTurn - strafeRight > 500) ? 500 : -ccwTurn - strafeRight;
+    rearControl = (-ccwTurn - strafeRight < -500) ? -500 : rearControl;
     
-    left_font_motor.writeMicroseconds(1500 - ccwturn + strafeRight);
-    left_rear_motor.writeMicroseconds(1500 - ccwturn - strafeRight);
-    right_rear_motor.writeMicroseconds(1500 - ccwturn - strafeRight);
-    right_font_motor.writeMicroseconds(1500 - ccwturn + strafeRight);
+    left_font_motor.writeMicroseconds(1500 + frontControl);
+    left_rear_motor.writeMicroseconds(1500 + rearControl);
+    right_rear_motor.writeMicroseconds(1500 + rearControl);
+    right_font_motor.writeMicroseconds(1500 + frontControl);
+    
     SerialCom -> print("angle = ");
     SerialCom -> println(angle);
     SerialCom -> print(", error = ");
     SerialCom -> println(error);
-    if((abs(ccwturn) > 20) ||(abs(strafeRight) > 20)){
-      t = millis() - t;
+    SerialCom -> println(t);
+    
+    //If the control signals are low for too long, start the timer to quit so that the motors don't burn out
+    if ((abs(strafeRight) < 50) && (abs(ccwTurn) < 50)){
+      t = t + millis() - tinit;
+    } else {
+      t = 0;
     }
   }
 }
